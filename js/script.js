0e0ba9d4fa1d61b10ca12814885f62f93c7950af
softvar <varun2902@gmail.com>
2024-05-23 19:44:19 +0530

    add more features

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ window.onload = function() {
   const input1Value = urlParams.get('a');
   const input2Value = urlParams.get('b');
 
+  function getInputValue(input) {
+    return document.getElementById(input).value;
+  }
+
   if (input1Value !== null) {
       document.getElementById('input1').value = input1Value;
   }
@@ -11,10 +15,28 @@ window.onload = function() {
       document.getElementById('input2').value = input2Value;
   }
 
-  document.getElementById('inputForm').addEventListener('submit', function(event) {
+  document.getElementById('inputForm1').addEventListener('submit', function(event) {
     event.preventDefault();  // Prevent the default form submission
-    const p = window.runjs.generateDeterministicHash(document.getElementById('input1').value, document.getElementById('input2').value);
+    const p = window.runjs.generateDeterministicHash(getInputValue('input1'), getInputValue('input2'));
 
     alert(p);
   });
+
+  document.getElementById('inputForm2').addEventListener('submit', function(event) {
+    event.preventDefault();  // Prevent the default form submission
+    const p = window.runjs.generateDeterministicHash(document.getElementById('input1').value, document.getElementById('input2').value);
+
+    const result = getInputValue('input3') + (p || getInputValue('input4')) + getInputValue('input5')
+
+    console.log(result);
+  });
 };
+
+function togglePassword(inputId) {
+    const input = document.getElementById(inputId);
+    if (input.type === "password") {
+        input.type = "text";
+    } else {
+        input.type = "password";
+    }
+}
